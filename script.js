93477a21e9518313f374096fcd39e2375aa8d2ef
Matthew Orme <matt.orme2010@gmail.com>
2019-10-27 20:28:53 +0000

    functional

---
+++ script.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+    el: '#app',
+
+    data: {
+        artist: 'Aerosmith',
+        title: 'Dream On',
+        lyrics: '',
+    },
+
+    methods: {
+        async getLyrics() {
+            try{
+            const response = await axios.get('https://api.lyrics.ovh/v1/' + this.artist + '/' + this.title);
+            this.lyrics = response.data.lyrics;
+            console.log(response);
+            }catch(error){
+                console.log(error);
+            }
+        }
+    },
+});
